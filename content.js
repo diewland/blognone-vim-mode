84f3c7e91849da53b8a00b7f0db2071ea7ea5a34
diewland <diewland@gmail.com>
2016-04-16 16:12:03 +0700

    bind node page

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
-var CURSOR = 0;
+// ########## CONSTANTS ##########
+
 var KEY_ETR = 13;
 var KEY_DOT = 46;
 var KEY_SLSH = 47;
 var KEY_QUES = 63;  // ? ( shift + / )
 var KEY_G = 71;     // G ( shift + g )
+var KEY_C = 99;
 var KEY_H = 104;
 var KEY_J = 106;
 var KEY_K = 107;
@@ -12,11 +14,28 @@ var KEY_O = 111;
 var KEY_P = 112;
 var KEY_R = 114;
 
+var CODE_ESC = 27;
+
+// ########## VARIABLES ##########
+
 var CURSOR = 0;
 
-$('body').keypress(function(evt){
-    //console.log(evt.which);
-    //console.log(evt.keyCode);
+// ########## FUNCTIONS ##########
+
+function is_list_screen(){
+    return location.href.match(/node\/.+/) == null;
+}
+
+function is_node_screen(){
+    return location.href.match(/node\/.+/) != null;
+}
+
+function handle_list_screen(evt){
+
+    // ignore keyboard event if not focus on body
+    if(document.activeElement.tagName != 'BODY'){
+        return;
+    }
 
     if(evt.which == KEY_J){ // down
         if(CURSOR < $('.node').length-1){ CURSOR += 1; }
@@ -94,5 +113,99 @@ $('body').keypress(function(evt){
     else if((evt.which == KEY_K)&&(rel_offset < 50)){ // close to top
         // console.log('up', abs_offset, win_height, abs_offset - (win_height/2), 'x');
         window.scrollTo({ left: 0, top: abs_offset - (win_height/2), behavior: 'smooth' });
+    }
+}
+
+function handle_node_screen(evt){
+
+    // ignore keyboard event if not focus on body
+    if(document.activeElement.tagName != 'BODY'){
+        return;
+    }
+
+    // gather content + comment
+    var items = $.merge( $('.node'), $('.comment')); 
+
+    if(evt.which == KEY_J){ // down
+        if(CURSOR < items.length-1){ CURSOR += 1; }
+    }
+    else if(evt.which == KEY_K){ // up
+        if(CURSOR > 0){ CURSOR -= 1; }
+    }
+    /*
+    else if(evt.which == KEY_C){ // add comment
+        window.scrollTo({ left: 0, top: $('#comment-area').offset().top, behavior: 'smooth' });
+        $('#comment-area textarea').focus();
+    }
+    */
+    else if(evt.which == KEY_R){ // reply
+        var reply_link = $(items).eq(CURSOR).find('.comment-links a');
+        if(reply_link.length == 1){
+            location.href = reply_link.attr('href');
+        }
+    }
+    else if(evt.which == KEY_DOT){ // top
+        CURSOR = 0;
+        window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+    }
+    else if(evt.which == KEY_G){ // bottom
+        CURSOR = items.length - 1;
+        window.scrollTo({ left: 0, top: $(items).last().offset().top, behavior: 'smooth' });
+    }
+    else if(evt.which == KEY_P){ // share g+
+        var url = 'https://plus.google.com/share?url=' + location.href;
+        window.open(url, 'gplus', "height=500,width=500,left=600,top=200");
+    }
+    else if(evt.which == KEY_QUES){ // help
+        var msg = ''
+                + '=== CURSOR ===\n'
+                + '\n'
+                + 'K - Move Up\n'
+                + 'J - Move Down\n'
+                + '. - Move Top\n'
+                + 'SHIFT+G - Move Bottom\n'
+                + '\n'
+                + '=== OTHER ===\n'
+                + '\n'
+                // + 'C - Add new comment\n'
+                + 'R - Reply\n'
+                + 'P - Share to Google+\n'
+                ;
+        alert(msg);
+    }
+
+    // update cursor
+    $(items).removeClass('active');
+    $(items).eq(CURSOR).addClass('active');
+
+    // handle scroll
+    var win_height = window.innerHeight;
+    var abs_offset = $('.active').offset().top;
+    var rel_offset = abs_offset - window.scrollY;
+    //
+    // console.log( win_height, rel_offset, abs_offset);
+    //
+    if((evt.which == KEY_J)&&(rel_offset + 100 > win_height)){ // close to end
+        // console.log('down', abs_offset, abs_offset-50, 'x');
+        window.scrollTo({ left: 0, top: abs_offset - 50, behavior: 'smooth' });
+    }
+    else if((evt.which == KEY_K)&&(rel_offset < 50)){ // close to top
+        // console.log('up', abs_offset, win_height, abs_offset - (win_height/2), 'x');
+        window.scrollTo({ left: 0, top: abs_offset - (win_height/2), behavior: 'smooth' });
+    }
+}
+
+// ########## BIND BODY EVENT ##########
+
+$('body').keypress(function(evt){
+    //console.log(evt.which);
+    //console.log(evt.keyCode);
+
+    // bind event
+    if(is_list_screen()){
+        handle_list_screen(evt);
+    }
+    else if(is_node_screen()){
+        handle_node_screen(evt);
     }
 });
